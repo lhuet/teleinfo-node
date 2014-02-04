a9405e2134b6535de9d9f17e60d137848d40dae1
Laurent HUET <laurent.huet@gmail.com>
2014-02-04 23:28:53 +0100

    fix tab spaces

--- example_teleinfo.js
+++ example_teleinfo.js
@@ -4,9 +4,9 @@ var util = require('util');
 var trameEvents = teleinfo("/dev/ttyAMA0");
 
 trameEvents.on('tramedecodee', function (data) {
-  console.log(util.inspect(data));
+	console.log(util.inspect(data));
 });
 
 trameEvents.on('error', function (err) {
-  console.log(util.inspect(err));
+	console.log(util.inspect(err));
 });
--- teleinfo.js
+++ teleinfo.js
@@ -7,38 +7,37 @@ function teleinfo(port) {
 	var trameEvents = new events.EventEmitter();
 
 	var serialPort = new serialport.SerialPort(port, {
-		  baudrate: 1200,
-		  dataBits: 7,
-		  parity: 'even',
-		  stopBits: 1,
-		  // Caractères séparateurs = fin de trame + début de trame
-		  parser: serialport.parsers.readline(String.fromCharCode(13,3,2,10))
-		});
+		baudrate: 1200,
+		dataBits: 7,
+		parity: 'even',
+		stopBits: 1,
+		// Caractères séparateurs = fin de trame + début de trame
+		parser: serialport.parsers.readline(String.fromCharCode(13,3,2,10))
+	});
 
 	serialPort.on('data', function(data) {
 		trameEvents.emit('trame', data);
 	});
 
-        serialPort.on('error', function(err) {
-                trameEvents.emit('error', err);
-        });
+	serialPort.on('error', function(err) {
+		trameEvents.emit('error', err);
+	});
 
 	trameEvents.on('trame', function(data) {
 		// Decode trame '9 lignes en tarif bleu base'
 		var trame = {};
 		var arrayOfData = data.split('\r\n');
 		for (var i=0; i < arrayOfData.length; i++) {
-		  decodeLigne(arrayOfData[i], trame, trameEvents);
+			decodeLigne(arrayOfData[i], trame, trameEvents);
 		}
 		// trame incomplete s'il manque la première ligne ADCO
 		if (!(trame.ADCO===undefined)) {
-		  trameEvents.emit('tramedecodee', trame);
+			trameEvents.emit('tramedecodee', trame);
 		}
 		else {
-                  var err = new Error('Trame incomplete');
-                  trameEvents.emit('error', err);
+			var err = new Error('Trame incomplete');
+			trameEvents.emit('error', err);
 		}
-		
 	});
 
 	return trameEvents;
@@ -46,43 +45,43 @@ function teleinfo(port) {
 
 
 function decodeLigne(ligneBrute, trame, trameEvents) {
-    // Ligne du type "PAPP 00290 ," (Etiquette / Donnée / Checksum)
-    var elementsLigne = ligneBrute.split(' ');
-    if (elementsLigne.length === 3) {
-      // Spec chk : somme des codes ASCII + ET logique 03Fh + ajout 20 en hexadécimal
-      // Résultat toujours un caractère ASCII imprimable allant de 20 à 5F en hexadécimal
-      // Checksum calculé sur etiquette+space+données => retirer les 2 derniers caractères
-      var sum = 0;
-      for (var j=0; j < ligneBrute.length-2; j++) {
-        sum += ligneBrute.charCodeAt(j);
-      }
-      sum = (sum & 63) + 32;
-      if (sum === ligneBrute.charCodeAt(j+1)) {
-        // Checksum ok -> on ajoute la propriété à la trame
-        // Conversion en valeur numérique pour certains cas
-        switch (elementsLigne[0].substr(0,4)) {
-          case 'BASE': // Index Tarif bleu
-          case 'HCHC': // Index Heures creuses
-          case 'HCHP': // Index Heures pleines
-          case 'EJPH': // Index EJP (HN et HPM)
-          case 'BBRH': // Index Tempo (HC/HP en jours Blanc, Bleu et Rouge)
-          case 'ISOU': // Intensité souscrite
-          case 'IINS': // Intensité instantannée (1/2/3 pour triphasé)
-          case 'ADPS': // Avertissement de dépassement
-          case 'IMAX': // Intensité max appelée (1/2/3 pour triphasé)
-          case 'PAPP': // Puissance apparente
-          case 'PMAX': // Puissance max triphasée atteinte
-            trame[elementsLigne[0]]= Number(elementsLigne[1]);
-            break;
-          default:
-            trame[elementsLigne[0]]= elementsLigne[1];
-        }
-        return true;
-      } else {
-        var err = new Error('Erreur de checksum : \n' + ligneBrute + '\n Checksum calculé/reçu : ' + sum + ' / ' + ligneBrute.charCodeAt(j+1)); 
-        trameEvents.emit('error', err);
-      }
-    };
+	// Ligne du type "PAPP 00290 ," (Etiquette / Donnée / Checksum)
+	var elementsLigne = ligneBrute.split(' ');
+	if (elementsLigne.length === 3) {
+		// Spec chk : somme des codes ASCII + ET logique 03Fh + ajout 20 en hexadécimal
+		// Résultat toujours un caractère ASCII imprimable allant de 20 à 5F en hexadécimal
+		// Checksum calculé sur etiquette+space+données => retirer les 2 derniers caractères
+		var sum = 0;
+		for (var j=0; j < ligneBrute.length-2; j++) {
+			sum += ligneBrute.charCodeAt(j);
+		}
+		sum = (sum & 63) + 32;
+		if (sum === ligneBrute.charCodeAt(j+1)) {
+			// Checksum ok -> on ajoute la propriété à la trame
+			// Conversion en valeur numérique pour certains cas
+			switch (elementsLigne[0].substr(0,4)) {
+				case 'BASE': // Index Tarif bleu
+				case 'HCHC': // Index Heures creuses
+				case 'HCHP': // Index Heures pleines
+				case 'EJPH': // Index EJP (HN et HPM)
+				case 'BBRH': // Index Tempo (HC/HP en jours Blanc, Bleu et Rouge)
+				case 'ISOU': // Intensité souscrite
+				case 'IINS': // Intensité instantannée (1/2/3 pour triphasé)
+				case 'ADPS': // Avertissement de dépassement
+				case 'IMAX': // Intensité max appelée (1/2/3 pour triphasé)
+				case 'PAPP': // Puissance apparente
+				case 'PMAX': // Puissance max triphasée atteinte
+					trame[elementsLigne[0]]= Number(elementsLigne[1]);
+					break;
+				default:
+					trame[elementsLigne[0]]= elementsLigne[1];
+			}
+			return true;
+		} else {
+			var err = new Error('Erreur de checksum : \n' + ligneBrute + '\n Checksum calculé/reçu : ' + sum + ' / ' + ligneBrute.charCodeAt(j+1)); 
+			trameEvents.emit('error', err);
+		}
+	};
 };
 
 
